99843c72ab80d4a8323aa6a1bd39571d1faae849
isfak <isfakiqbalchowdhury@gmail.com>
2025-05-18 22:08:59 +0600

    `Added Offer and NewCollections components to Shop page`

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+
+export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
+  return (
+    <div className='navbar'>
+      <div className="navlogo">
+        <img src={logo} alt="logo" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="navmenu">
+        <li onClick={() => setMenu("shops")}>
+          <Link style={{textDecoration:'none'}} to="/">Shop</Link>
+          {menu === "shops" && <hr />}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link style={{textDecoration:'none'}} to="/mens">Men</Link>
+          {menu === "mens" && <hr />}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          <Link style={{textDecoration:'none'}} to="/womens">Women</Link>
+          {menu === "womens" && <hr />}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link style={{textDecoration:'none'}} to="/kids">Kids</Link>
+          {menu === "kids" && <hr />}
+        </li>
+      </ul>
+      <div className="nav-log-in-cart">
+        <Link style={{textDecoration:'none'}} to="/login"><button>Login</button></Link>
+        <Link style={{textDecoration:'none'}} to="/cart"><img src={cart_icon} alt="cart" /></Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
