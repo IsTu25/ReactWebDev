b35fb548bef035cb9843caa3eb6b53017cfbd899
isfak <isfakiqbalchowdhury@gmail.com>
2025-05-23 01:44:11 +0600

    first commit

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
 import { Popular } from '../Components/Popular/Popular'
+import { Offer } from '../Components/Offer/Offer'
+import { NewCollections } from '../Components/NewCollections/NewCollections'
 
 export const Shop = () => {
   return (
     <div>
         <Hero/>
         <Popular/>
+        <Offer/>
+        <NewCollections/>
     </div>
   )
 }
